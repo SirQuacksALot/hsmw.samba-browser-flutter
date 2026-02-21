62e4c1c87f449208111fe2e104774cb56a39f8a7
sebastian <sebastian.glass3@gmail.com>
2026-02-21 14:37:16 +0100

    Refactor SMBClient to use async/await pattern

--- ios/Classes/SMBClient.swift
+++ ios/Classes/SMBClient.swift
@@ -1,85 +1,37 @@
-
 import AMSMB2
 
 class SMBClient {
-    /// connect to: `smb://guest@XXX.XXX.XX.XX/share`
-    
     let serverURL: URL
     let credential: URLCredential
     let share: String
     
-    lazy private var client = AMSMB2(url: self.serverURL, credential: self.credential)!
+    lazy private var client = SMB2Manager(url: self.serverURL, credential: self.credential)!
     
     init(url: String, share: String, user: String, password: String) {
         serverURL = URL(string: url)!
         self.share = share
-        credential = URLCredential(user: user, password: password, persistence: URLCredential.Persistence.forSession)
+        credential = URLCredential(user: user, password: password, persistence: .forSession)
     }
     
-    private func connect(handler: @escaping (Result<AMSMB2, Error>) -> Void) {
-        client.connectShare(name: self.share) { error in
-            if let error = error {
-                handler(.failure(error))
-            } else {
-                handler(.success(self.client))
-            }
+    private func connect() async throws -> SMB2Manager {
+        try await client.connectShare(name: self.share)
+        return self.client
+    }
+    
+    func listDirectory(path: String) async throws -> [String] {
+        let client = try await connect()
+        let files = try await client.contentsOfDirectory(atPath: path)
+        return files.compactMap { entry -> String? in
+            guard let rawName = entry[.pathKey] as? String else { return nil }
+            let fileType = entry[.fileResourceTypeKey] as? URLFileResourceType
+            let suffix = fileType == .directory ? "/" : ""
+            return serverURL.absoluteString + "/" + rawName + suffix
         }
     }
     
-    func listDirectory(path: String, handler: @escaping (Result<[String], Error>) -> Void) {
-        
-        connect { result in
-            switch result {
-            case .success(let client):
-                client.contentsOfDirectory(atPath: path) { result in
-                    switch result {
-                    case .success(let files):
-                        var shares: [String] = []
-                        for entry in files {
-                            let fileType: URLFileResourceType = entry[.fileResourceTypeKey] as! URLFileResourceType
-                            var rawName: String = entry[.pathKey] as! String
-                            if (fileType == URLFileResourceType.directory) {
-                              rawName += "/"
-                            }
-                            
-                            shares.append(self.serverURL.absoluteString + "/" + rawName)
-                        }
-                        
-                        handler(.success(shares))
-                        
-                    case .failure(let error):
-                        handler(.failure(error))
-                    }
-                }
-                
-            case .failure(let error):
-                handler(.failure(error))
-            }
-        }
+    func downloadFile(atPath: String, to: String) async throws -> String {
+        let client = try await connect()
+        try await client.downloadItem(atPath: atPath, to: URL(fileURLWithPath: to))
+        return to
     }
-    
-    func downloadFile(atPath: String, to: String, handler: @escaping (Result<String, Error>) -> Void) {
-        connect { result in
-            switch result {
-            case .success(let client):
-                client.downloadItem(atPath: atPath, to: URL(fileURLWithPath: to)) { bytes, total in
-                    return true
-                } completionHandler: { error in
-                    switch error {
-                    case .some(let error):
-                        handler(.failure(error))
-                        
-                    case .none:
-                        handler(.success(to))
-                    }
-                }
-                
-            case .failure(let error):
-                print(error)
-                handler(.failure(error))
-            }
-        }
-    }
-    
-    
 }
